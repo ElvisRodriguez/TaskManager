f7bc6333e2ee5264cbb7b878fa88fbe34f626b84
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2019-08-25 21:52:22 -0400

    edited utils, added __pycache__ to git ignore

--- HelperTools/utils.py
+++ HelperTools/utils.py
@@ -2,7 +2,13 @@
 Module for useful constants and functions.
 '''
 
-#Constants
+# Constants
 
 LOWER_ALPHABET = ''.join([chr(i) for i in range(97, 123)])
 UPPER_ALPHABET = ''.join([char.upper() for char in LOWER_ALPHABET])
+
+# Functions
+
+def print_iterable(iterable, sep=' '):
+    print(*iterable, sep=sep)
+
